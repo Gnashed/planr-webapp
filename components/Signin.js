18569433b8e740938dc97f7c6f0cc83fa68f2ab9
Tion Blackmon <tionblackmon@icloud.com>
2024-02-20 21:09:56 -0600

    Completed planr-webapp issue #1

--- components/Signin.js
+++ components/Signin.js
@@ -4,19 +4,11 @@ import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
+    <div className="planr-landing-page-bg">
+      <h1>Planr</h1>
+      <h2>The only tool you will need for guiding the next generation.</h2>
       <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
-        Sign In
+        Get started
       </Button>
     </div>
   );
