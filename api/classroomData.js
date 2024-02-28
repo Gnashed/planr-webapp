5491cf020405c8c534fcc00b9c36836ad184f9fd
Tion Blackmon <tionblackmon@icloud.com>
2024-02-28 12:43:51 -0600

    Created pHTTP requests as promises in /api

---
+++ api/classroomData.js
@@ -0,0 +1,31 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+// TODO: CREATE Classroom
+const createClassroom = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/#######`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getClassroom = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/###/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+export { createClassroom, getClassroom };
