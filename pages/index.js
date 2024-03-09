40bf447de37abb5a106b510a800aa3f71e5c1d55
Tion Blackmon <tionblackmon@icloud.com>
2024-03-09 10:28:53 -0500

    Using index.js to link to other pages

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,48 @@
-// // import { signOut } from '../utils/auth';
-// import { Button } from 'react-bootstrap';
-// import Link from 'next/link';
+// // // import { signOut } from '../utils/auth';
 
-import React, { useEffect, useState } from 'react';
-import StudentCard from '../components/StudentCard';
-import { getStudents } from '../api/studentData';
+// import React, { useEffect, useState } from 'react';
+// import StudentCard from '../components/StudentCard';
+// import { getStudents } from '../api/studentData';
+// // import { Button } from 'react-bootstrap';
+// // import Link from 'next/link';
 
-function Home() {
-  const [students, setStudents] = useState([]);
+// function Home() {
+//   const [students, setStudents] = useState([]);
 
-  const fetchStudents = () => {
-    getStudents().then(setStudents);
-  };
+//   const fetchStudents = () => {
+//     getStudents().then(setStudents);
+//   };
 
-  useEffect(() => {
-    fetchStudents();
-  }, []);
+//   useEffect(() => {
+//     fetchStudents();
+//   }, []);
+
+//   return (
+//     <div
+//       className="text-center d-flex flex-column justify-content-center align-content-center"
+//       style={{
+//         height: '90vh',
+//         padding: '30px',
+//         maxWidth: '400px',
+//         margin: '0 auto',
+//       }}
+//     >
+//       <h2>Classroom</h2>
+
+//       {students.map((student) => (
+//         <StudentCard key={student.firebaseKey} studentObj={student} onUpdate={fetchStudents} />
+//       ))}
+//     </div>
+//   );
+// }
+
+// export default Home;
+import { Button } from 'react-bootstrap';
+import Link from 'next/link';
+import { useAuth } from '../utils/context/authContext';
+
+export default function Home() {
+  const { user } = useAuth();
 
   return (
     <div
@@ -27,13 +54,14 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h2>Classroom</h2>
+      <h1>Hello {user.displayName}! </h1>
 
-      {students.map((student) => (
-        <StudentCard key={student.firebaseKey} studentObj={student} onUpdate={fetchStudents} />
-      ))}
+      <Link href="/classroom/new" passHref>
+        <Button variant="success">Create Classroom</Button>
+      </Link>
+      <Link href="/classroom" passHref>
+        <Button variant="primary" className="mt-1">Classrooms</Button>
+      </Link>
     </div>
   );
 }
-
-export default Home;
